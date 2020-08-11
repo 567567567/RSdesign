f9a55331024353384f44913794a374c0ec1b22d2
567567567 <69498532+567567567@users.noreply.github.com>
2020-08-11 19:06:38 +0900

    RSdesign 8/11
    初アップロード

---
+++ RSdesign持ち帰り用/mailto.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>送信完了画面</title>
+	</head>
+	<body>
+		<h2>メール送信完了</h2>
+		
+		<p class="message">お問い合わせありがとうございます。近日中にご返信させていただきます。</p>
+	</body>
+</html>
